410c345075c1f6cafdec400c711a42576bfa4ae5
Kyan Hartadi <kyanra.hartadi@gmail.com>
2025-07-05 15:28:33 +0700

    finance scraper
    day 2

---
+++ finance scraper/scraping_test.py
@@ -0,0 +1,4 @@
+from testing import testing
+
+if __name__ == "__main__":
+    testing()
---
+++ finance scraper/testing.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+def testing():
+    
+    resp = requests.get('https://finance.detik.com') #url
+
+    #print(resp.content)
+    #print(resp.status_code)
+
+    soup = BeautifulSoup(resp.content, 'html.parser')
+
+    #print(soup)
+
+    rows = soup.select(".datafx .datafx__item") #datafx__item
+
+    
+
+    for r in rows:
+        title = r.select_one(".datafx__item--title").get_text(strip=True)
+        value = r.select_one(".datafx__item--value").get_text(strip=True)
+        changes = r.select_one(".datafx__item--changes").get_text(strip=True)
+        percent = r.select_one(".datafx__item--percent").get_text(strip=True)
+
+        print(f"{title} \n {value} \n Change: {changes} \n Percent: {percent} \n")
